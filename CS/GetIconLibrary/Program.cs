edae36c7edd604c347073282b729013ec74b58ba
lnu <l.nullens@gmail.com>
2020-06-04 09:49:47 +0200

    19.2.5 icons list upgrade to VS2019 migration to .net framework 4.8 support for svg added

--- CS/GetIconLibrary/Program.cs
+++ CS/GetIconLibrary/Program.cs
@@ -3,19 +3,26 @@ using System.Drawing;
 using System.IO;
 using DevExpress.Images;
 using DevExpress.Utils.Design;
+using DevExpress.Utils.Svg;
 
-namespace GetIconLibrary {
+namespace GetIconLibrary
+{
 
-    class Program {
-        static void Main(string[] args) {
+    class Program
+    {
+        static void Main(string[] args)
+        {
 
             string filePath = @"c:\DevExpressIcons\";
 
-            if (!Directory.Exists(filePath)) {
-                try {
+            if (!Directory.Exists(filePath))
+            {
+                try
+                {
                     Directory.CreateDirectory(filePath);
                 }
-                catch (Exception e) {
+                catch (Exception e)
+                {
                     Console.WriteLine("Can't create a directory: {0}", e.ToString());
                 }
                 finally { }
@@ -26,12 +33,28 @@ namespace GetIconLibrary {
 
         }
 
-        public static void StoreImagesByImageType(string filePath, ImageType imageType) {
-            foreach (ImagesAssemblyImageInfo imageInfo in ImagesAssemblyImageList.Images) {
+        public static void StoreImagesByImageType(string filePath, ImageType imageType)
+        {
+            foreach (ImagesAssemblyImageInfo imageInfo in ImagesAssemblyImageList.Images)
+            {
                 var stream = ImageResourceCache.Default.GetResourceByFileName(imageInfo.Name, imageType);
                 if (stream == null) continue;
-                using (Image image = Image.FromStream(stream)) {
-                    image.Save(filePath + imageInfo.Name);
+                var newFilePath = Path.Combine(filePath, imageInfo.ImageType.ToString());
+                if (!Directory.Exists(newFilePath))
+                {
+                    Directory.CreateDirectory(newFilePath);
+                }
+                if (imageInfo.ImageType != ImageType.Svg)
+                {
+                    using (Image image = Image.FromStream(stream))
+                    {
+                        image.Save(Path.Combine(newFilePath, imageInfo.Name));
+                    }
+                }
+                else
+                {
+                    SvgImage image = SvgImage.FromStream(stream);
+                    image.Save(Path.Combine(newFilePath, imageInfo.Name));
                 }
                 Console.WriteLine("Writing " + imageInfo.Name);
             }
